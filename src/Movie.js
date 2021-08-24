fb01edcd0c8ede7207d0123c3deb1c3d749c6686
kuk329 <kuk329@cau.ac.kr>
2021-08-24 19:26:54 +0900

    before css

---
+++ src/Movie.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+function Movie({id, year, title,summary,poster }){
+    return(
+        <div class="movie">
+            <img src={poster} title={title} alt={title}/>
+            <div class="movie__data">
+                <h3 class="movie__title">{title}</h3>
+                <h5 class="movie__year">{year}</h5>
+                <p class="movie__summary">{summary}</p>
+            </div>
+        
+        </div>
+    );
+};
+
+Movie.propTypes = {
+    id:PropTypes.number.isRequired,
+    year:PropTypes.number.isRequired ,
+    title: PropTypes.string.isRequired,
+    summary : PropTypes.string.isRequired,
+    poster : PropTypes.string.isRequired
+}
+
+export default Movie;
